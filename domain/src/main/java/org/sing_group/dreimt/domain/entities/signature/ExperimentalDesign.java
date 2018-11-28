7050c986cd7b0c91b27290c98ac151615df693c0
Hugo López-Fernández <hlfernandez@uvigo.es>
2018-11-28 12:34:58 +0100

    Update possible experimental design values

--- domain/src/main/java/org/sing_group/dreimt/domain/entities/signature/ExperimentalDesign.java
+++ domain/src/main/java/org/sing_group/dreimt/domain/entities/signature/ExperimentalDesign.java
@@ -23,5 +23,5 @@
 package org.sing_group.dreimt.domain.entities.signature;
 
 public enum ExperimentalDesign {
-  IN_VIVO, EX_VIVO, IN_VITRO, IN_SILICO, PATIENT, TRANSFECTION
+  IN_VIVO, EX_VIVO, IN_VITRO, IN_SILICO, PATIENT, TRANSFECTION, UNKNOWN
 }
